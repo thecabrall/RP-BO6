3d66d248eeec9a4a4a40185d4de1a9c577e5d2d0
thecabrall <gruposppider@gmail.com>
2024-09-04 23:06:25 -0300

    v1.05

--- RankedPro.py
+++ RankedPro.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 import ranking as rk
 
 st.set_page_config(page_title='RankedPro', page_icon=None)
@@ -17,11 +18,14 @@ h1,h2,h3 = st.columns(3)
 #--------| MEIO | --------#
 body = st.container()
 a1,a2,a3,a4 = st.columns(4)
+b1,b2 = st.columns([4,2])
+
+if 'pt' not in st.session_state:
+    st.session_state['pt'] = []
 
 with head:
     with h2:
         logo = st.image('ranked-white.png')
-
 
 
 with body:
@@ -33,5 +37,13 @@ with body:
         res = pf - pi
         rank,logo = rk.rank(pf)
         stats = st.metric(label='Ranking Atual',value=f'{rank}',delta=f'{res}')
+        st.session_state['pt'].append(pf)
+
     with a4:
         st.image(f'{logo}')
+    with b1:
+        graf = st.line_chart(data=st.session_state['pt'])
+    with b2:
+        apagar = st.link_button('Apagar Dados','https://rankedpro.streamlit.app')
+        if apagar == True:
+            st.session_state['pt'].clear()
